5bd49a8ddd12382b4402c197f60ddad6ad67e214
MehdiVazifedan <46728500+MehdiVazifedan@users.noreply.github.com>
2019-01-16 13:32:37 -0500

    Add files via upload

---
+++ R/get_breakshbr.R
@@ -0,0 +1,7 @@
+get_breakshbr <-
+function(ehat,B,eps=(.Machine$double.eps)^0.5) {
+  breaks <- quantile(ehat,seq(0,1,length=B))
+  ind <- c(1,length(breaks))
+  breaks[ind] <- breaks[ind] + eps*sd(breaks)*c(-1,1)
+  unique(breaks)
+}
